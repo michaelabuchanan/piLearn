7b4b5efa949566a4e15dd23dbe4643e73d147a21
buchanam <buchanam@oregonstate.edu>
2020-10-05 12:09:55 -0700

    starting to get site set up

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const http = require('http');
+const http = require('http'),
+      express = require('express');
 
 const hostname = '192.168.0.30';
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  switch (req.url) {
-   case "/home":
-     res.writeHead(200);
-     res.end("<h1>This is Home page</h1>");
-     break;
-   case "/about":
-     res.writeHead(200);
-     res.end("<h1>This is About page</h1>");
-     break;
-   default:
-     res.writeHead(200);
-     res.end("<h1>Raspberry Pi DL Demo!</h1>");
-     break;
- }
-});
+// Initialize express app and http server
+const app = express();
+const http = http_module.Server(app);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.set('port', port); // set express to use this port
+
+app.use(express.static(path.join(__dirname, 'pages', 'build')));
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   switch (req.url) {
+//    case "/home":
+//      res.writeHead(200);
+//      res.end("<h1>This is Home page</h1>");
+//      break;
+//    case "/about":
+//      res.writeHead(200);
+//      res.end("<h1>This is About page</h1>");
+//      break;
+//    default:
+//      res.writeHead(200);
+//      res.end("<h1>Raspberry Pi DL Demo!</h1>");
+//      break;
+//  }
+// });
+
+const server = http.listen(app.get('port'), () => {
+      console.info(`==> 🌎  Go to ` + HOST + `:${app.get('port')}`);
+    });
+
+const io = require('socket.io').listen(server);
---
+++ pages/src/home.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+
+export default () => {
+  return(
+    <h1>HELLOOOOO</h1>
+  );
+
+};

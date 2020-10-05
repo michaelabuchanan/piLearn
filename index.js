27727ed11961aab4ca012f58b68e0c43444a543c
buchanam <buchanam@oregonstate.edu>
2020-10-05 13:58:30 -0700

    trying things

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'pages', 'build')));
 // });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'src', 'index.html'));
 });
 
 const server = http.listen(app.get('port'), () => {

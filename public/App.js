750c21f4d13e0f631c85c93e34e6db49375014de
michaelabuchanan <buchanam@oregonstate.edu>
2020-10-09 13:43:37 -0700

    testing things

--- public/App.js
+++ public/App.js
@@ -6,28 +6,29 @@ import Host from './Host';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <div class="container-fluid">
-        <Switch>
-          <Route exact path="/">
-            <Host />
-          </Route>
-          {/* <Route path="/data">
-            <Data />
-          </Route>
-          <Route path="/live-stream/host">
-            <Host />
-          </Route>
-          <Route path="/live-stream/:id?">
-            <LiveStream />
-          </Route>
-          <Route>
-            <Error />
-          </Route> */}
-        </Switch>
-      </div>
-    </Router>
+      <h1>HELLOOOO</h1>
+    // <Router>
+    //   <Header />
+    //   <div class="container-fluid">
+    //     <Switch>
+    //       <Route exact path="/">
+    //         <Host />
+    //       </Route>
+    //       {/* <Route path="/data">
+    //         <Data />
+    //       </Route>
+    //       <Route path="/live-stream/host">
+    //         <Host />
+    //       </Route>
+    //       <Route path="/live-stream/:id?">
+    //         <LiveStream />
+    //       </Route>
+    //       <Route>
+    //         <Error />
+    //       </Route> */}
+    //     </Switch>
+    //   </div>
+    // </Router>
   );
 }
 

6e5ada5c591de2eafbcabfd6994122a9b394e375
michaelabuchanan <buchanam@oregonstate.edu>
2020-10-09 10:42:50 -0700

    more changes

--- server.js
+++ server.js
@@ -36,12 +36,11 @@ app.use(express.static(__dirname + '/public'));
 //app.get('*', (req, res) => {
 //  res.sendFile(path.join(__dirname, 'pages', 'src', 'index.html'));
 //});
-app.set('appPath', 'public');
 
-
-app.all('*', function(req, res) {
-      res.sendfile(__dirname+'/public/index.html')
-})
+app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'public', 'build', 'index.html'));
+    });
+    
 
 const server = http.listen(app.get('port'), () => {
       console.info(`==> 🌎  Go to ` + hostname + `:${app.get('port')}`);

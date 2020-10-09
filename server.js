892658ae7e33ac080b834fd978cd8f4031abc4ae
michaelabuchanan <buchanam@oregonstate.edu>
2020-10-09 13:53:00 -0700

    trying more things

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'public',  'index.html'));
+      res.sendFile(path.join(__dirname, 'public',  'app.js'));
     });
     
 
